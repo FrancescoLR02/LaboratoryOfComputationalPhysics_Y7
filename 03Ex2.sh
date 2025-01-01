8fee7fccc3f0d69ba1bbf785ffdca16596c96700
Francesco <franci192002@gmail.com>
2025-01-01 12:43:10 +0100

    ex03

---
+++ 03Ex2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+filename='data.csv'
+newFilename='data.txt'
+evenNumber=0
+
+#! important!! 
+
+#Deleting metadata, sub the comma and saving the file
+grep -v "^#" $filename | tr -d "," > $newFilename
+
+#counting even number:
+
+#TODO
+
+
+#X, Y, Z in module less or grater than 100*sqrt(3/2)
+grater=0
+lesser=0
+#th=100*sqrt(3/2)
+
+newFilename="file1 file2 file3"  # Space-separated list
+
+
+for i in $newFilename; do
+
+   echo $i
+   done
+
+
