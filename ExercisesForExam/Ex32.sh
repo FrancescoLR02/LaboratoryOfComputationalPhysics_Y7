ea9cae77b2b9661740533e35b101fe4e1b156020
Francesco <franci192002@gmail.com>
2025-01-18 11:12:30 +0100

    ex3, ex2

--- ExercisesForExam/Ex32.sh
+++ ExercisesForExam/Ex32.sh
@@ -1,58 +1,63 @@
-#!/bin/bash
+#!/bin/bash 
 
 fileName="data.csv"
-newFileName="data.txt"
-
-grep -v "^#" "$fileName" | tr -d "," > "$newFileName"
-
-#2)
-
-IsEven=0
-
-awk '{ for(i=1; i <=NF; ++i ) if ($i%2 == 0) IsEven++} END {print IsEven }' "$newFileName"
+NewFileName="data.txt"
 
 
-counts=$(grep -c -v "^#" "$fileName")
-greater=0
+#Delete the commas and metadata:
+
+grep -v "^#" "$fileName" | tr -d "," > "$NewFileName"
+
+#How many even numbers:
+
+isEven=0
+
+awk '{ for(i=1; i<=NF; i++) if ($i % 2 == 0) isEven++ } END {print isEven}' $NewFileName
+
+
+#Distinguish the entries on the basis:
+
+grater=0
 smaller=0
 
-sqrt=$( echo "scale=4; sqrt(3/2)" | bc)
-number=100
+counts=$(grep -c -v "^#" $fileName)
 
-th=$(echo "$number * $sqrt" |bc )
+sqrt=$(echo "scale=4; sqrt(3/2)" | bc)
+variable=100
+Th=$(echo "$variable * $sqrt" | bc)
 
-
-X=$(cut -d "," -f1 "$fileName" | tail -n +5)
-Y=$(cut -d "," -f2 "$fileName" | tail -n +5)
-Z=$(cut -d "," -f3 "$fileName" | tail -n +5)
-
+X=$(cut -d "," -f1 "$fileName" | tail -n +5 )
+Y=$(cut -d "," -f2 "$fileName" | tail -n +5 )
+Z=$(cut -d "," -f3 "$fileName" | tail -n +5 )
 
 for i in $(seq 1 $counts); do
 
-   X_i=$(echo "$X" |sed -n "${i}p")
-   Y_i=$(echo "$Y" |sed -n "${i}p")
-   Z_i=$(echo "$Z" |sed -n "${i}p")
+   X_i=$(echo "$X" | sed -n "${i}p")
+   Y_i=$(echo "$Y" | sed -n "${i}p")
+   Z_i=$(echo "$Z" | sed -n "${i}p")
 
-   EuclideanDistance=$(echo "scale=4; sqrt($X_i^2 + $Y_i^2 + $Z_i^2)" | bc)
+   Distance=$(echo "scale=4; sqrt($X_i^2 + $Y_i^2 + $Z_i^2)" | bc )
 
-   if (( $(echo "$EuclideanDistance > $th" | bc) )); then
-      greater=$((greater + 1))
-
+   if (( $(echo "$Distance > $Th" | bc) )); then
+      grater=$((grater + 1))
    else
       smaller=$((smaller + 1))
 
-   fi
-   
+   fi 
+
+done 
+
+echo "The total number of value that are grater then 100*sqrt(3/2) is: $grater"
+echo "The total number of value that are less then 100*sqrt(3/2) is: $smaller"
+
+
+#Make n copies
+
+read -p "number of copies: " n
+
+for i in $(seq 1 $n); do
+   awk -v div="$i" '{for(j=1; j<=NF; ++j) $j = $j / div }1' "$NewFileName" > "data_${i}.txt"
 done
 
-echo "The number of numbers for which the euclidean distance is grater than sqrt(3/2)*100 is: $greater"
-echo "The number of numbers for which the euclidean distance is smaller than sqrt(3/2)*100 is: $smaller"
 
 
-read -p "Number of copies: " n
-
-for i in $(seq 1 $n); do 
-
-   awk -v div="$i" '{for (j=1; j<=NF; ++j) $j = $j /div}1' "$newFileName" > "data_${i}.txt"
-
-done
